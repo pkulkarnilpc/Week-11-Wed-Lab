782c764b5f2e9aa69a487b32124acb85c74884b9
pkulkarnilpc <pkulkarni@laspositascollege.edu>
2025-10-29 09:54:04 -0700

    Update LinearProbing.hpp

--- LinearProbing.hpp
+++ LinearProbing.hpp
@@ -6,22 +6,12 @@ class LinearProbing : public HashTableClosed<T> {
 private:
     int skip; // skip factor
 public:
-    explicit LinearProbing(int size = 101, int skipFactor = 3)
+    LinearProbing(int size = 101, int skipFactor = 3)
         : HashTableClosed<T>(size), skip(skipFactor) {}
 
-    // TODO: Implement linear probing with skip
+    
     int probeIndex(const T& key, int i) const override {
-        // TO DO
+        // TODO: Implement linear probing with skip
         return 0;
-    }
-
-    int insert(const T& key) override {
-        // TO DO
-        return 0;
-    }
-
-    pair<bool, int> search(const T& key) const override {
-        // TO DO
-        return {false, 0};
     }
 };
